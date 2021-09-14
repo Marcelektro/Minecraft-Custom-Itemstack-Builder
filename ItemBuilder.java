ac3e795c8e60e7a2fb382d979dfb43262beb54e9
Marcelektro <32849144+Marcelektro@users.noreply.github.com>
2021-09-14 09:53:55 +0200

    Update and rename ItemManager.java to ItemBuilder.java

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -12,93 +12,93 @@ import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("unused")
-public class ItemManager {
+public class ItemBuilder {
     private final ItemStack item;
     private final List<String> lore = new ArrayList<>();
     private final ItemMeta meta;
 
-    public ItemManager(Material mat, short subId, int amount) {
+    public ItemBuilder(Material mat, short subId, int amount) {
         this.item = new ItemStack(mat, amount, subId);
         this.meta = this.item.getItemMeta();
     }
 
-    public ItemManager(ItemStack item) {
+    public ItemBuilder(ItemStack item) {
         this.item = item;
         this.meta = item.getItemMeta();
     }
 
-    public ItemManager(Material mat, short subId) {
+    public ItemBuilder(Material mat, short subId) {
         this.item = new ItemStack(mat, 1, subId);
         this.meta = this.item.getItemMeta();
     }
 
-    public ItemManager(Material mat, int amount) {
+    public ItemBuilder(Material mat, int amount) {
         this.item = new ItemStack(mat, amount, (short) 0);
         this.meta = this.item.getItemMeta();
     }
 
-    public ItemManager(Material mat) {
+    public ItemBuilder(Material mat) {
         this.item = new ItemStack(mat, 1, (short) 0);
         this.meta = this.item.getItemMeta();
     }
 
-    public ItemManager setAmount(int value) {
+    public ItemBuilder setAmount(int value) {
         this.item.setAmount(value);
         return this;
     }
 
-    public ItemManager setNoName() {
+    public ItemBuilder setNoName() {
         this.meta.setDisplayName(" ");
         return this;
     }
 
-    public ItemManager setGlow() {
+    public ItemBuilder setGlow() {
         this.meta.addEnchant(Enchantment.DURABILITY, 1, true);
         this.meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         return this;
     }
 
-    public ItemManager setData(short data) {
+    public ItemBuilder setData(short data) {
         this.item.setDurability(data);
         return this;
     }
 
-    public ItemManager addLoreLine(String line) {
+    public ItemBuilder addLoreLine(String line) {
         this.lore.add(line);
         return this;
     }
 
-    public ItemManager addLoreArray(String[] lines) {
+    public ItemBuilder addLoreArray(String[] lines) {
         this.lore.addAll(Arrays.asList(lines));
         return this;
     }
 
-    public ItemManager addLoreAll(List<String> lines) {
+    public ItemBuilder addLoreAll(List<String> lines) {
         this.lore.addAll(lines);
         return this;
     }
 
-    public ItemManager setDisplayName(String name) {
+    public ItemBuilder setDisplayName(String name) {
         this.meta.setDisplayName(name);
         return this;
     }
 
-    public ItemManager setSkullOwner(String owner) {
+    public ItemBuilder setSkullOwner(String owner) {
         ((SkullMeta) this.meta).setOwner(owner);
         return this;
     }
 
-    public ItemManager setUnbreakable(boolean value) {
+    public ItemBuilder setUnbreakable(boolean value) {
         this.meta.spigot().setUnbreakable(value);
         return this;
     }
 
-    public ItemManager addEnchantment(Enchantment enchantment, int lvl) {
+    public ItemBuilder addEnchantment(Enchantment enchantment, int lvl) {
         this.meta.addEnchant(enchantment, lvl, true);
         return this;
     }
 
-    public ItemManager addItemFlag(ItemFlag flag) {
+    public ItemBuilder addItemFlag(ItemFlag flag) {
         this.meta.addItemFlags(flag);
         return this;
     }
